e2c6ec942afb52ef53c7a205d2e1135a4215ae22
abdullah <abdullahsalameh66@gmail.com>
2022-06-30 17:37:42 +0300

    Add login page & firebase 💖

---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+import firebase from "firebase/app";
+import 'firebase/auth';
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+export const auth = firebase.initializeApp({
+    apiKey: "AIzaSyBUpzR5tHeILL4Y4wHK0wIajBabNZX-y5M",
+    authDomain: "messenger-clone-83978.firebaseapp.com",
+    projectId: "messenger-clone-83978",
+    storageBucket: "messenger-clone-83978.appspot.com",
+    messagingSenderId: "642791738125",
+    appId: "1:642791738125:web:b745c20f134f336f187e44",
+    measurementId: "G-W1K2Q515R8"
+  });
